9914de13a9381b94691578c9c07a1eeff835eaad
Kurtis Angell <KurtisBlow@MacBook-Pro.local>
2019-01-31 16:12:25 +0000

    completed

--- models/news.js
+++ models/news.js
@@ -1,21 +1,18 @@
-const  { emotionalAnalysisModel } = require('../models/watson');
+const { emotionalAnalysisModel } = require("../models/watson");
 
-exports.newsAnalysisModel = (news) => {
+exports.newsAnalysisModel = news => {
   return new Promise((resolve, reject) => {
-
     let parsedNews = JSON.parse(news.text).articles;
 
     parsedNews = parsedNews.reduce((acc, value) => {
       acc.push({
-        content : value.content,
-        contenttype: 'application/json',
-        language: 'en'
-      })
-      return acc
-    }, [])
+        content: value.content,
+        contenttype: "application/json",
+        language: "en"
+      });
+      return acc;
+    }, []);
 
-    resolve(emotionalAnalysisModel(JSON.stringify(parsedNews)))
-
-    // cannot resolve until emotionalAnalysisModel is evaluated
-  })
-}
+    resolve(emotionalAnalysisModel(JSON.stringify(parsedNews)));
+  });
+};
