6f5eb3a7c52d03d9bd1229dbbf67e49285cd5acc
Kurtis Angell <kurtisangell@gmail.com>
2019-02-23 17:44:07 +0000

    Update goodReads.js

--- controllers/goodReads.js
+++ controllers/goodReads.js
@@ -41,23 +41,3 @@ exports.authorAnalysis = (req, res, next) => {
         });
     });
 };
-
-// return request
-//   .get(
-//     "https://www.goodreads.com/author/show.xml?key=slEl1SJHVTgMjOwIttW9Gw&id=128382"
-//   )
-//   .then(data => {
-//     //needs to be returned
-//     return authorAnalysisModel(data.body + "");
-//   })
-//   .then(watsonData => {
-//     const result = [watsonData.personality[1], watsonData.personality[4]];
-
-//     // //render to ejs
-//     // res.render('goodReads', { result }, (err, renderedGoodReads) => {
-//     // // });
-
-//     res.render("goodReads.ejs", { result });
-
-//     // res.send(result);
-//   });
